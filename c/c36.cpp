883fa3adff9237cb4f93ee679e2e189585e2bab6
yanyuw <547274419@qq.com>
2017-12-06 21:26:01 +0800

    pat 1028

---
+++ c/c36.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int n,i,sum,year,month,day;
+    int minyear,minmonth,minday,maxyear,maxmonth,maxday;
+    char name[5],old[5],young[5];
+    sum = 0;
+    maxyear = maxmonth = maxday = 0;
+    minyear = 2014;
+    minmonth = 9;
+    minday = 6;
+
+    scanf("%d",&n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%s %d/%d/%d",name,&year,&month,&day);
+
+        if((year<2014 || (year==2014 && month<9) || (year==2014 && month==9 && day<=6)) && (year>1814 || (year==1814 && month>9)|| (year==1814 && month==9 && day>=6)))
+        {
+            sum++;
+            if(year<minyear || (year==minyear && month<minmonth) || (year==minyear && month==minmonth && day<minday))
+            {
+                strcpy(old,name);
+                minyear = year;
+                minmonth = month;
+                minday = day;
+            }
+            if(year>maxyear || (year==maxyear && month>maxmonth) || (year==maxyear && month==maxmonth && day>maxday))
+            {
+                strcpy(young,name);
+                maxyear = year;
+                maxmonth = month;
+                maxday = day;
+            }
+        }
+
+    }
+    
+    printf("%d %s %s",sum,old,young);
+    return 0;
+}
